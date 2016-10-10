e23a8c4cb7bf1b2ff29f4f54f9f8f0df3d726251
Shuft <shuft@coils.work>
2016-10-10 23:47:56 +0900

    PulseReadTest.inoの内容を更新

--- PulseReadTest/PulseReadTest.ino
+++ PulseReadTest/PulseReadTest.ino
@@ -1,16 +1,21 @@
+/*
+ * PulseReadTest.ino
+ * 
+ * [How to use]
+ * 1. Compile and Write to arduino.
+ * 2. Open Serial Monitor or Serial Plotor
+ */
+
 //assign pulse read pin num.
 const int PulseIn = 0;
-const int led_pin = 9;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(PulseIn,INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int pulse = analogRead(PulseIn);
   Serial.println(pulse);
-  delay(500);
+  delay(10);
 }
